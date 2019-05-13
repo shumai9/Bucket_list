9c45db95c06d339092ad591051dde16a161f29a2
Shumai <shumai9@github.com>
2019-05-13 22:08:25 +0100

    database for test and dev setup and migrated

--- db/migrate/20190513204821_create_users.rb
+++ db/migrate/20190513204821_create_users.rb
@@ -2,8 +2,8 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :username
-      t.string :email
-      t.string :password
+      t.string :first_name
+      t.string :last_name
 
       t.timestamps
     end
