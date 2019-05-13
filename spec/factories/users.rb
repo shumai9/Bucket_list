5bfd5360b940a835c32940967761214ed6df705e
Shumai <shumai9@github.com>
2019-05-14 00:43:00 +0100

    added request folder with files to test requests to api endpoints

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,9 @@
 FactoryBot.define do
   factory :user do
-    username { "MyString" }
-    email { "MyString" }
-    password { "MyString" }
+    user_name { Faker::Superhero.name}
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name}
+    email {  Faker::Internet.email}
+    password { Faker::PhoneNumber.phone_number }
   end
 end
