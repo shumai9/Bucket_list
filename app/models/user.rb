38a30202764636807b2268b2689eb44b284f2eb3
Shumai <shumai9@github.com>
2019-05-15 18:29:55 +0100

    modified:   Gemfile :-> added gem bcrypt 	modified:   app/models/user.rb :-> added password_digest 	modified:   spec/factories/users.rb :->added user setup and fixed password issues 	modified:   spec/models/user_spec.rb  :-> removed user name validation and added psd_digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
+  # encrypt password
+  has_secure_password
+  
+  validates_presence_of  :first_name, :last_name, :password_digest
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :registerable, :recoverable, :rememberable, :validatable
+
+  #devise auth commented so as jwt can take over 
+  #:database_authenticatable,
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :user do
-    user_name { "MyString" }
-    first_name { "MyString" }
-    last_name { "MyString" }
-    password_digest { "MyString" }
+    user_name { Faker::Superhero.name}
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name}
+    email {  Faker::Internet.email}
+    password{ Faker::PhoneNumber.phone_number }
   end
 end
